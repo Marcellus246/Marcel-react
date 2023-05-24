dd38d625e5be33cb1f5ca10c7f650d5f38f8b39c
Marcellus246 <125130831+Marcellus246@users.noreply.github.com>
2023-05-24 15:40:32 +0000

    gass

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 const Navbar1 = () => {
   return (
     <div className="animate__animated animate__fadeInDown animate__delay-1s">
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="success" variant="dark">
         <Container>
           <Navbar.Brand href="#" className="fw-bold">Marcel House</Navbar.Brand>
           {/* <Nav className="me-auto">
